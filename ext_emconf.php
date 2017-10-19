b36bdfa9f487671ffe9bfcbe046043556038b5a7
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2017-10-19 16:14:17 +0200

    [INIT] Basic extension files
    * add composer.json
    * add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Versatile Crawler',
+	'description' => 'A versatile and extendable crawler',
+	'category' => 'plugin',
+	'author' => 'Thorben Nissen',
+	'author_email' => 'thorben.nissen@kapp-hamburg.de',
+	'author_company' => '',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '1.0.0-dev',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '8.7.0-8.99.99',
+		),
+		'conflicts' => array(
+		    'crawler' => ''
+		),
+		'suggests' => array(
+		),
+	),
+);
