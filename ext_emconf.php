000dacf23fea05e8945beaa4f00f0ae6c35cef55
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2019-01-27 20:28:53 +0100

    [TASK] Begin with version 1.1.0
    * remove version from `composer.json`
    * adjust dependency for `typo3/cms-core`
    * set version in `ext_emconf.php` to `1.1.0-dev`

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.0.1',
+	'version' => '1.1.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-8.99.99',
+			'typo3' => '8.7.0-9.5.99',
 		),
 		'conflicts' => array(
 		    'crawler' => ''
